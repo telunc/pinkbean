2cefe0ff11761e01e3ad15bbf71bb8e686401a02
Andy Chen <telunc@sfu.ca>
2017-09-30 01:34:22 -0700

    Update Ping

--- src/structures/commands/ping.js
+++ src/structures/commands/ping.js
@@ -1,7 +1,7 @@
 export default (message) => {
     let start = message.createdTimestamp;
     message.channel.send('Pink Bean responded').then((message) => {
-        let diff = (message.createdTimestamp - start);
+        let diff = Math.abs(message.createdTimestamp - start);
         message.edit(`Pink Bean responded in *${diff/1000} seconds*`);
     }).catch((error) => console.log(error));
 };
