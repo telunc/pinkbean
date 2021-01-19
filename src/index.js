bc3eceb47aa6c76fa26e45256fefe666a2b0c1b0
Andy <telunc@sfu.ca>
2021-01-18 20:50:09 -0800

    try single shard for now

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import { ShardingManager } from 'discord.js';
 import config from 'config';
 
-const Manager = new ShardingManager('./lib/client.js', {totalShards: 4, token: config.get('discord').token});
+const Manager = new ShardingManager('./lib/client.js', {totalShards: 1, token: config.get('discord').token});
 Manager.spawn();
